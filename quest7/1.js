ccac2eb46361e61f434fc89a5f2d1a26a6d4c3e7
Daniel30102001 <fernando30102001@gmail.com>
2025-01-26 17:31:18 -0300

    new

--- quest7/1.js
+++
@@ -1,7 +0,0 @@
-//questao 1 completa
-
-const aluno = [
-       {  nome: 'joão',idade: 20 },
-       { curso: 'adiministraçao', nota: 9.8 }
-]
-console.log(aluno)
--- quest7/3.js
+++
@@ -1,8 +0,0 @@
-const livro = {
-       titulo: 'Dom Casmurro',
-       autor: 'Machado de Assis',
-       anodePublicação: 1899,
-}
-console.log(livro.titulo)
-console.log(livro.autor)
-
